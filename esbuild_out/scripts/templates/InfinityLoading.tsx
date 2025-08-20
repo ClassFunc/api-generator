a4696230afa749b5a6f3c1e1f7a49bb4328f965b
LE VAN THANH <zero.lethanh@gmail.com>
2025-08-20 18:25:02 +0700

    DataPathFieldConfig as string

--- esbuild_out/scripts/templates/InfinityLoading.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const InfinityLoading = () => {
-    return (
-        <div className="animate-pulse bg-slate-600 p-4 text-white">
-            Loading...
-        </div>
-    )
-}
